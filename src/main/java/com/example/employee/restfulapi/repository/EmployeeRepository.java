c23bd198e3cee08b4427a659ce9f2e07415ed088
wan000si <576529493@qq.com>
2018-06-08 22:13:27 +0800

    modify EployeeRepository

--- src/main/java/com/example/employee/restfulapi/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/restfulapi/repository/EmployeeRepository.java
@@ -3,8 +3,10 @@ package com.example.employee.restfulapi.repository;
 import com.example.employee.restfulapi.entity.Employee;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.awt.print.Pageable;
 import java.util.List;
 
@@ -21,8 +23,12 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Employee save(Employee employee);
 
+    @Transactional
+    @Modifying
     int update(long id, String name, Integer age, String gender, Integer salary, long companyId);
 
+    @Transactional
+    @Modifying
     void deleteByName(String name);
 
 }
