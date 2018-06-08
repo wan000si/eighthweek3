ab6541289600c9ae774709f28360eb0140a9ccfa
wan000si <576529493@qq.com>
2018-06-08 22:12:38 +0800

    add EployeeRepository

--- src/main/java/com/example/employee/restfulapi/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/restfulapi/repository/EmployeeRepository.java
@@ -1,9 +1,28 @@
 package com.example.employee.restfulapi.repository;
 
 import com.example.employee.restfulapi.entity.Employee;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    @Override
+    List<Employee> findAll();
+
+    Employee findById(long id);
+
+    Page<Employee> finAll(Pageable pageable);
+
+    List<Employee> findByGender(String gender);
+
+    Employee save(Employee employee);
+
+    int update(long id, String name, Integer age, String gender, Integer salary, long companyId);
+
+    void deleteByName(String name);
+
 }
