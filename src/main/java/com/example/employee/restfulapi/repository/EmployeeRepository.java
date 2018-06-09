fb245b9fdaaf3a58209bb94373642610f6251a0e
wan000si <576529493@qq.com>
2018-06-09 21:23:16 +0800

    modify  EmployeeRepository

--- src/main/java/com/example/employee/restfulapi/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/restfulapi/repository/EmployeeRepository.java
@@ -1,31 +1,37 @@
 package com.example.employee.restfulapi.repository;
 
+import com.example.employee.restfulapi.entity.Company;
 import com.example.employee.restfulapi.entity.Employee;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
-import java.awt.print.Pageable;
+//import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     @Override
+    @Query("select e from Employee e")
     List<Employee> findAll();
 
+    @Query("select e from Employee e where e.id=?1")
     Employee findById(long id);
 
-    Page<Employee> finAll(Pageable pageable);
+    Page<Employee> findAll(Pageable pageable);
 
     List<Employee> findByGender(String gender);
 
     Employee save(Employee employee);
 
-    @Transactional
     @Modifying
-    int update(long id, String name, Integer age, String gender, Integer salary, long companyId);
+    @Transactional
+    @Query("update Employee e set e.name=?2,e.age=?3,e.gender=?4,e.companyId=?5,e.salary=?6 where e.id=?1")
+    void update(Long id, String name, Integer age, String gender, Long companyId, Integer salary);
 
     @Transactional
     @Modifying
